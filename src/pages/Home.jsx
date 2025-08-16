bcec88bb89581fa964ee308f8a4e0f41a4b0dd13
Appu <aroodhkalloli2003@gmail.com>
2025-08-17 00:56:20 +0530

    Updated code or changes description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import { StarBackground } from "../components/StarBackgound"
 import { ThemeToggle } from "../components/ThemeToggle"
 import { Navbar } from "../components/Navbar"
 import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
+import { SkillsSection } from "../components/SkillSection"
+import { ProjectsSection } from "../components/ProjectSections"
+import { ContactSection } from "@/components/ContactSection";
 export const Home=() => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -11,6 +15,10 @@ export const Home=() => {
         <Navbar/>
         <main>
             <HeroSection/>
+            <AboutSection/>
+            <SkillsSection/>
+            <ProjectsSection/>
+            <ContactSection/>
         </main>
         </div>
     )
